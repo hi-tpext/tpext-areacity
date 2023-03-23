b81abf3ee270b47471c4a03d05a79c232871c85c
ichynul <ichynul@163.com>
2023-03-23 20:13:40 +0800

    version

--- common/Module.php
+++ common/Module.php
@@ -6,7 +6,7 @@ use tpext\common\Module as baseModule;
 
 class Module extends baseModule
 {
-    protected $version = '1.0.2';
+    protected $version = '1.0.3';
 
     protected $name = 'tpext.areacity';
 
